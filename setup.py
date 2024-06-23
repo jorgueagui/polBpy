4c47e6c934ea61cc96dc74fbeea73ceb98cce29c
Jordan Guerra <jordan.guerra@gmail.com>
2024-06-23 09:24:34 -0600

    Jun 23  2024

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polBpy",
-    version="0.1.0",
+    version="0.1.1",
     author="Jordan Guerra",
     author_email="jordan.guerra@gmail.com",
     description="Package for performing angular dispersion analysis of polarimetric data\
